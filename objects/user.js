848e23f7d6e1e258f3d6a76b81a95369720932d5
Shreyansh Mishra <shreyanshsaru@gmail.com>
2020-08-17 21:25:47 +0530

    Improve User class

--- objects/user.js
+++ objects/user.js
@@ -1,16 +1,36 @@
 const user = require('../models/user.js');
+const Database = require('./database.js');
+const CryptoJS = require('crypto-js')
 
-class User{
-    
-    constructor(data){
+const secretKey = process.env.cryptosecret || "your-crypto-secret-key"
+
+class User {
+
+    constructor(data) {
         this.data = data;
     }
 
-    async createUser(){
+    async createUser() {
+
+        let flag = 1;
         let newUser = new user(this.data);
-        await newUser.save(); 
-        console.log("A new user has been registered. The Data is as follows:\n"+newUser);
+        let cryptedPassword = CryptoJS.AES.encrypt(this.data.password, secretKey)
+        newUser.password = cryptedPassword;
+        await newUser.save((err, user) => {
+            if (err) {
+                console.log("An error occured while saving your data:\n" + err);
+                flag = 0;
+            } else {
+                console.log("A new user has been registered. The Data is as follows:\n" + newUser);
+            }
+        });;
+        if (flag === 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
+
 }
 
 module.exports = User;
