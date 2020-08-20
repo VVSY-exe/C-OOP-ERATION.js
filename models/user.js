2129b13f2375411ac433b3b0af8a94841b8d3b82
Shreyansh Mishra <shreyanshsaru@gmail.com>
2020-08-20 21:04:25 +0530

    add functionality for signup, show abstraction and inheritance

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 //import the MongoDB module 
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
-
+let jwt = require('jsonwebtoken');
 //import dotenv module
 require('dotenv').config();
 
@@ -13,8 +13,23 @@ mongoose.connect(process.env.db, { useNewUrlParser: true, useUnifiedTopology: tr
 let User = new Schema({
     name: {type: String, required: true},
     password: {type: String, required: true},
-    username: {type: String, required: true}
+    username: {type: String, required: true},
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
+
+User.methods.generateAuthToken = async function() {
+    // Generate an auth token for the user
+    const user = this;
+    const token = jwt.sign({_id: user._id}, process.env.JWT_KEY);
+    user.tokens = user.tokens.concat({token});
+    await user.save();
+    return token;
+}
 
 //create a mongodb model with the name 'User' and Schema User
 let user = mongoose.model('User', User);
