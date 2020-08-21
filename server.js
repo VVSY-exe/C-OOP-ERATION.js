bef7b1df00d31c96ab70511e7c585444670981b0
Shreyansh Mishra <shreyanshsaru@gmail.com>
2020-08-21 22:23:16 +0530

    implement login session system

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ const express = require('express');
 //import body parser, to handle JSON in request bodys
 var bodyParser = require('body-parser');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 //create an express app
 const app = express();
 const User = require('./objects/user.js');
 const Database = require('./objects/database.js');
 const userdb = require('./models/user.js');
+const authenticateToken = require('./middleware/authenticate.js');
+var cookieParser = require('cookie-parser');
+const user = require('./models/user.js');
+app.use(cookieParser());
 //make express use body-parser's json
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -25,12 +30,13 @@ app.set('view engine', 'ejs');
 
 
 //GET request handler for homepage
-app.get('/', function (req, res) {
-    let db = new User({"boomer": "lord"});
-    res.status(200).send(db);
-    db.getData().boomer = "nopb"
-    console.log(db.getData());
-
+app.get('/', authenticateToken, function (req, res) {
+    if(req.user!=null){
+    res.send(`You are logged in as ${req.user.name}`);
+    }
+    else{
+        res.send('You are not logged in, please login to view content');
+    }
 })
 
 //GET request handler for Signup Page
@@ -40,48 +46,58 @@ app.get('/signup', (req, res) => {
 
 //POST request handler for Signup, adds an User to the DB by creating an Object
 app.post('/signup', async (req, res) => {
-    const user = await new User (req.body);
+    const user = await new User(req.body);
     let status = await user.createUser();
     console.log(status)
-    if(!status){
+    if (!status) {
         res.send('You failed to register!')
-    }
-    else{
+    } else {
         res.send(`Success!
         <script>
         setTimeout(function () {
            // after 2 seconds
-           window.location = "/";
+           window.location = "/login";
         }, 2000)
       </script>`);
     }
 })
 
-app.get('/login', async(req, res) => {
-    res.status(200).render(__dirname+'/public/views/login/login.ejs');
+app.get('/login', async (req, res) => {
+    res.status(200).render(__dirname + '/public/views/login/login.ejs');
 })
 
-app.post('/login', async(req, res) => {
-    //Login a registered user
-    try {
-        const { username, password } = req.body;
-        const user = await new User().findExisting(username, CryptoJS.AES.encrypt(password, secretKey));
-        if (!user) {
-            return res.status(401).send({error: 'Login failed! Check authentication credentials'});
-        }
-        const token = await new user.generateAuthToken();
-        console.log("h"+ user, token)
-        res.send({ user, token });
-    } catch (error) {
-        res.status(400).send(error);
+app.post('/login', async (req, res) => {
+    //authenticate user 
+    const {username,password} = req.body;
+    let userObj = await new User(req.body);
+    let user = await userObj.loginCheck(username, password);
+    await userObj.generateToken(user,res);
+    
+})
+
+app.get('/logout',authenticateToken,async (req,res)=>{
+    if(req.user!=null){
+        await new User().logout(req.user,req,res);  //polymorphism
+        res.redirect('/');
     }
-
+    else{
+        res.redirect('/');
+    }
 })
 
+app.get('/logoutall',authenticateToken,async(req,res)=>{
+    if(req.user!=null){
+        await new User().logout(req.user,req,res,true);   //polymorphism
+        res.redirect('/');
+    }
+    else{
+        res.redirect('/');
+    }
+})
 
-app.get('/showdb',async (req,res)=>{
+app.get('/showdb', async (req, res) => {
     let request = await new Database();
-    let db = await request.showdb();
+    let db = await request.showdb({});
     res.status(200)
     res.type('json');
     res.send("User Database:\n\n" + JSON.stringify(db, null, "\t"))
