494bb5f671ab1ea76b6e34a6d47deab66219b3c0
Shreyansh Mishra <shreyanshsaru@gmail.com>
2020-08-22 15:51:45 +0530

    fix logoutall function

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -11,9 +11,23 @@ async function authenticateToken(req,res,next){
     
     try{
         const verified = jwt.verify(token,process.env.JWT_KEY)
-        console.log(verified);
+        let users = await new User().showdb();
+        let tokens = [];
+        users.forEach(user=>{
+            user.tokens.forEach(t=>{
+                tokens.push(t._id);
+            })
+        })
+        let existing = tokens.find(t=>t===token);
+        if(existing){
         req.user = await new User().showdb({'_id': verified});
         next();    
+        }
+        else{
+            req.user=null;
+            next();
+            return req.user;
+        }
     }catch(err){
         res.send('An error occured while verifying token:\n'+err);
         next();
