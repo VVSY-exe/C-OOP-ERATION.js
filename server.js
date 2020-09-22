325213d98fe6c580e2abce604fd96fc8c8fbed17
Shreyansh Mishra <shreyanshsaru@gmail.com>
2020-09-22 17:50:15 +0530

    move code to classes so as to show OOPs concepts

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ app.use(bodyParser.urlencoded({
 }));
 var fileupload = require('express-fileupload');
 app.use(fileupload());
-var Profilephotos = require('./models/profilephotos.js')
+var Profilephotos = require('./objects/profilephotos.js');
+const post = require('./objects/post.js');
 //set json spaces to 2, used when serving JSON data
 app.set('json spaces', 2);
 //make public a static directory, to serve static files
@@ -35,28 +36,15 @@ app.set('view engine', 'ejs');
 
 
 //GET request handler for homepage
-//not object oriented
+//OOPs impelemented
 app.get('/', authenticateToken, async function (req, res) {
     if (req.user != null) {
-        let following = [];
-        let database = await new User().getModel('user');
-        let post = await posts.find({
-            'id': req.user._id
-        });
-        for (ele of req.user.following) {
-            let name = await database.findById(ele.friend);
-            await following.push(name.name);
-
-        }
-        let profilephoto = await Profilephotos.findOne({
-            "id": req.user._id.toString()
-        })
-        profilephoto = profilephoto.profilephoto;
+        let obj = await new User().getDashboard(req);
         res.render((__dirname + '/public/views/dashboard/dashboard.ejs'), {
-            user: req.user,
-            following,
-            profilephoto,
-            post,
+            user: obj.user,
+            following: obj.following,
+            profilephoto: obj.profilephoto,
+            post: obj.post,
         });
     } else {
         res.render(__dirname + '/public/views/notLoggedInPage/notloggedin.ejs');
@@ -70,18 +58,14 @@ app.get('/signup', (req, res) => {
 })
 
 //POST request handler for Signup, adds an User to the DB by creating an Object
-//partially not object oriented
+//object oriented
 app.post('/signup', async (req, res) => {
     const user = await new User(req.body);
     let status = await user.createUser();
     if (!status) {
         res.send('You failed to register!')
     } else {
-        let pfp = new Profilephotos({
-            id: await status._id,
-            profilephoto: req.files.profilephoto.data.toString("base64")
-        });
-        await pfp.save();
+        await new Profilephotos().assignProfilePhoto(req, status);
         res.send(`Success!
         <script>
         setTimeout(function () {
@@ -137,63 +121,26 @@ app.get('/showdb', async (req, res) => {
     res.send("User Database:\n\n" + JSON.stringify(db, null, "\t"))
 })
 
+//OOPs Implemented
 app.get('/users', authenticateToken, async (req, res) => {
     if (req.user != null) {
-        let user = req.user;
-        let users = await new User().showdb('User');
-        let profilepic = []
-        users = users.filter(function (obj) {
-            return obj.name !== user.name;
-        });
-        let following = []
-        for (user of users) {
-            let flag = 0;
-            for (follows of req.user.following) {
-                if (follows.friend == user._id) {
-                    following.push(true);
-                    flag = 1;
-                    break;
-                }
-            }
-            if (flag === 0) {
-                following.push(false);
-            }
-        }
-
-        for (user of users) {
-            profilepic.push(await Profilephotos.findOne({
-                'id': user._id
-            }))
-
-        }
-
+        let obj = await new User().getFollowingList(req);
         res.render(__dirname + '/public/views/users/users.ejs', {
-            users,
-            user: req.user,
-            profilepic,
-            following
+            users: obj.users,
+            user: obj.user,
+            profilepic: obj.profilepic,
+            following: obj.following
         });
+    }
+    else{
+        res.redirect('/');
     }
 })
 
+//OOPs Implemented
 app.post('/follow/:id', authenticateToken, async (req, res) => {
     if (req.user != null) {
-        let user = req.user;
-        let followuser = await new User().showdb('User', {
-            '_id': req.params.id
-        });
-        if (followuser != null) {
-            followuser.followers.push({
-                'friend': user._id
-            });
-            await followuser.save();
-            user.following.push({
-                'friend': followuser._id
-            });
-            await user.save();
-        } else {
-            res.send("User does not exist.")
-        }
+        await new User().followUser(req,res); 
     } else {
         res.redirect('/login')
     }
@@ -252,28 +199,19 @@ app.post('/removelike/:id', authenticateToken, async (req, res) => {
     }
 })
 
-app.get('/comments/:id', async (req, res) => {
-    let post = await posts.findOne({
-        '_id': req.params.id
-    });
-    let by = []
-    let user = []
-    post.comments.forEach(ele => {
-        by.push(ele.by);
-    })
-    for (ele of by) {
-        user.push(await new User().showdb('user', {
-            '_id': ele
-        }));
-
+//OOPs Implemented
+app.get('/comments/:id',authenticateToken, async (req, res) => {
+    if(req.user!=null){
+    let obj = await new Post().getComment(req);
+    console.log(obj.post,obj.user)
+    res.render(__dirname + '/public/views/comments/comments.ejs', {post: obj.post,user: obj.user});
     }
-    console.log('user', user)
-    res.render(__dirname + '/public/views/comments/comments.ejs', {
-        post,
-        user
-    });
+    else{
+        res.redirect('/');
+    }
 })
 
+//OOPs Implemented
 app.post('/postcomment/:id', authenticateToken, async (req, res) => {
     if (req.user != null) {
         await new Post().postComment(req);
