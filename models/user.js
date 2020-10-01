30d43777feeac6dd3b3b3d484a5192742ac65f98
Shreyansh Mishra <shreyanshsaru@gmail.com>
2020-10-01 17:49:44 +0530

    add new fields which are necessary for pages

--- models/user.js
+++ models/user.js
@@ -12,8 +12,12 @@ mongoose.connect(process.env.db, { useNewUrlParser: true, useUnifiedTopology: tr
  *whenever a new user signs up this creates a new user object in the database*/
 let User = new Schema({
     name: {type: String, required: true},
-    password: {type: String, required: true
-        , index:true, sparse:true},
+    bio: {type: String, required: false, default: "Lorem Ipsum"},
+    about1: {type: String, default: null},
+    about2: {type: String, default: null},
+    about3: {type: String, default: null},
+    isPage: {type: Boolean, default: false},
+    password: {type: String, required: true, index:true, sparse:true},
     username: {type: String, required: true, index:true, sparse:true},
     following: [{
         _id: 0,
